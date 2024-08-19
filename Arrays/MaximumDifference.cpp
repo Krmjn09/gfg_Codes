8450e4bedb53d63da677530dc53458df4b795218
Krmjn09 <kmahajan1_be22@thapar.edu>
2024-08-19 12:01:49 +0530

    commited

--- Arrays/MaximumDifference.cpp
+++ Arrays/MaximumDifference.cpp
@@ -1,13 +1,41 @@
 #include<iostream>
 #include<climits>
 using namespace std;
+// this is the naive solution
+// int maxDifference(int a[], int n)
+// {
+//     int result = a[1] - a[0];
+//     for(int i=0;i<n-1;i++)
+//     {
+//         for(int j = i+1;j<n;j++)
+//         {
+//             result = max(a[j]-a[i],result);
+
+//         }
+//     }
+//     return result;
+// }
+
+// better solution with theta n 
 int maxDifference(int a[], int n)
 {
-    int maxDiff = a[1] - a[0];
-    int result = a[1] - a[0];
-    for(int i=1;i<n;i++)
+    int res = a[1] - a[0];
+    int minElement = a[0];
+    for(int i = 1;i<n;i++)
     {
-        maxDiff = max(maxDiff, )
-        
+        res = max(res, a[i]-minElement);
+        minElement = min(a[i],minElement);
+
     }
+    return res;
+
+
+}
+
+int main()
+{
+    int arr[7] = {2,3,11,16,4,8,1};
+    int r = maxDifference(arr,7);
+    cout<< r;
+    return 0;
 }
